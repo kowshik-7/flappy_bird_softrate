864a5d48ac10f32f8b8bcd465855a0987de21e44
kowshik-7 <127116685+kowshik-7@users.noreply.github.com>
2023-08-14 23:59:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-// // window.alert("hi")
 
-const sec1 = document.querySelectorAll(".section3");
+
 const sec2 = document.querySelectorAll(".phoneImg");
-const sec3 = document.querySelectorAll(".description");
 const sec4 = document.querySelectorAll(".phone");
-const sec5 = document.querySelectorAll(".descriptionHead");
-const sec6 = document.querySelectorAll(".details");
-const sec7 = document.querySelectorAll(".sec3Container");
 const clip = document.querySelectorAll(".clip");
-
-for (let i=0;i<sec1.length;i++){
-    sec1[i].addEventListener('mouseenter',function(e){
-        clip[i].play()
-    })
-    sec1[i].addEventListener('mouseout',function(e){
-        clip[i].pause()
-    })
-}
 
 for (let i=0;i<sec2.length;i++){
     sec2[i].addEventListener('mouseenter',function(e){
@@ -27,47 +13,12 @@ for (let i=0;i<sec2.length;i++){
     })
 }
 
-for (let i=0;i<sec3.length;i++){
-    sec3[i].addEventListener('mouseenter',function(e){
-        clip[i].play()
-    })
-    sec3[i].addEventListener('mouseout',function(e){
-        clip[i].pause()
-    })
-}
 
 for (let i=0;i<sec4.length;i++){
     sec4[i].addEventListener('mouseenter',function(e){
         clip[i].play()
     })
     sec4[i].addEventListener('mouseout',function(e){
-        clip[i].pause()
-    })
-}
-
-for (let i=0;i<sec5.length;i++){
-    sec5[i].addEventListener('mouseenter',function(e){
-        clip[i].play()
-    })
-    sec5[i].addEventListener('mouseout',function(e){
-        clip[i].pause()
-    })
-}
-
-for (let i=0;i<sec6.length;i++){
-    sec6[i].addEventListener('mouseenter',function(e){
-        clip[i].play()
-    })
-    sec6[i].addEventListener('mouseout',function(e){
-        clip[i].pause()
-    })
-}
-
-for (let i=0;i<sec7.length;i++){
-    sec7[i].addEventListener('mouseenter',function(e){
-        clip[i].play()
-    })
-    sec7[i].addEventListener('mouseout',function(e){
         clip[i].pause()
     })
 }
@@ -81,14 +32,43 @@ for (let i=0;i<clip.length;i++){
     })
 }
 
-// const sec = document.querySelectorAll(".section3");
-// const clip = document.querySelectorAll(".clip");
-
-// sec.addEventListener("mouseenter",function(e){
-//     clip.play()
-// })
+// const race = document.querySelector(".section2");
+// let racewidth = race.offsetWidth;
+// let amtToScroll = racewidth - window.innerWidth; 
 
 
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline({
+    scrollTrigger:{
+        trigger: ".section2",
+        start : "bottom 100%",
+        end : "bottom 0%",
+        scrub: 4,
+        pin : ".section2"
+        
+    }
+});
+
+tl.to(".flyingBird",{x:0,y:80})
+  .to(".flyingBird",{x:100,y:90})
+  .to(".flyingBird",{x:120,y:200})
+  .to(".flyingBird",{x:300,y:40})
+  .to(".flyingBird",{x:400,y:10})
+  .to(".flyingBird",{x:550,y:150})
+  .to(".flyingBird",{x:740,y:-30})
+  .to(".flyingBird",{x:980,y:90})
+  .to(".flyingBird",{x:1200,y:-10})
+  .to(".flyingBird",{x:1500,y:100})
 
 
 
+gsap.to(".section3",{
+  scrollTrigger:{
+      trigger: ".section3",
+      start:"top 0%",
+      end :"bottom 0%",
+      scrub: 2,
+      pin: ".section2"
+  }
+})  
